355ea7b1887d59c6ace2c957ea1593bc2f670a1a
jhernandez <jhernandez@grupomurchison.com.ar>
2022-04-30 18:25:59 -0300

    primer commit challenge1

---
+++ WebApplication27-4/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication27_4.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+    }
+    
+    
+}
